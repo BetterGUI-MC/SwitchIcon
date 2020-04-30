647a8502483b04c81d5111d0c48d5baaf0f4daa3
HSGamer <huynhqtienvtag@gmail.com>
2020-04-30 13:13:39 +0700

    implement

--- src/main/java/me/hsgamer/bettergui/switchicon/Main.java
+++ src/main/java/me/hsgamer/bettergui/switchicon/Main.java
@@ -1,49 +1,12 @@
 package me.hsgamer.bettergui.switchicon;
 
+import me.hsgamer.bettergui.builder.IconBuilder;
 import me.hsgamer.bettergui.object.addon.Addon;
 
 public final class Main extends Addon {
 
-  /**
-   * Called when loading the addon
-   *
-   * @return whether the addon is loaded properly
-   */
-  @Override
-  public boolean onLoad() {
-    return true;
-  }
-
-  /**
-   * Called when enabling the addon
-   */
   @Override
   public void onEnable() {
-    // Enable logic
-  }
-
-  /**
-   * Called after all addons were loaded
-   */
-  @Override
-  public void onPostEnable() {
-    // Post Enable logic
-  }
-
-  /**
-   * Called when disabling the addon
-   */
-  @Override
-  public void onDisable() {
-    // Disable logic
-  }
-
-
-  /**
-   * Called when reloading
-   */
-  @Override
-  public void onReload() {
-    // Reload logic
+    IconBuilder.register("switch", SwitchIcon.class);
   }
 }
---
+++ src/main/java/me/hsgamer/bettergui/switchicon/SwitchIcon.java
@@ -0,0 +1,84 @@
+package me.hsgamer.bettergui.switchicon;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.function.Consumer;
+import me.hsgamer.bettergui.lib.xseries.XMaterial;
+import me.hsgamer.bettergui.object.ClickableItem;
+import me.hsgamer.bettergui.object.Icon;
+import me.hsgamer.bettergui.object.Menu;
+import me.hsgamer.bettergui.object.ParentIcon;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryClickEvent;
+
+public class SwitchIcon extends Icon implements ParentIcon {
+
+  private final List<Icon> icons = new ArrayList<>();
+  private final Map<UUID, Integer> currentIndexMap = new HashMap<>();
+  private final Consumer<InventoryClickEvent> nextIndexConsumer = e -> currentIndexMap
+      .computeIfPresent(e.getWhoClicked().getUniqueId(), (uuid1, integer) -> {
+        if (integer + 1 >= icons.size()) {
+          return 0;
+        }
+        return integer + 1;
+      });
+
+  public SwitchIcon(String name, Menu<?> menu) {
+    super(name, menu);
+  }
+
+  public SwitchIcon(Icon original) {
+    super(original);
+  }
+
+  @Override
+  public void setFromSection(ConfigurationSection configurationSection) {
+    setChildFromSection(getMenu(), configurationSection);
+  }
+
+  @Override
+  public Optional<ClickableItem> createClickableItem(Player player) {
+    UUID uuid = player.getUniqueId();
+    currentIndexMap.putIfAbsent(uuid, 0);
+
+    Optional<ClickableItem> optional = icons.get(currentIndexMap.get(uuid))
+        .createClickableItem(player);
+    if (optional.isPresent()) {
+      ClickableItem clickableItem = optional.get();
+      return Optional.of(new ClickableItem(clickableItem.getItem(),
+          clickableItem.getClickEvent().andThen(nextIndexConsumer)));
+    }
+
+    return Optional.of(new ClickableItem(XMaterial.AIR.parseItem(), nextIndexConsumer));
+  }
+
+  @Override
+  public Optional<ClickableItem> updateClickableItem(Player player) {
+    UUID uuid = player.getUniqueId();
+
+    Optional<ClickableItem> optional = icons.get(currentIndexMap.get(uuid))
+        .updateClickableItem(player);
+    if (optional.isPresent()) {
+      ClickableItem clickableItem = optional.get();
+      return Optional.of(new ClickableItem(clickableItem.getItem(),
+          clickableItem.getClickEvent().andThen(nextIndexConsumer)));
+    }
+
+    return Optional.of(new ClickableItem(XMaterial.AIR.parseItem(), nextIndexConsumer));
+  }
+
+  @Override
+  public void addChild(Icon icon) {
+    icons.add(icon);
+  }
+
+  @Override
+  public List<Icon> getChild() {
+    return icons;
+  }
+}
